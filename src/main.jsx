b98cb8fa266e0d432811eeb82f39567955614ca4
Alexander Celis <dacelis0@misena.edu.co>
2022-01-25 20:48:17 -0500

    retear los archivos de vite, crear un componente básico

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import { Blog } from './pages/Blog'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Blog />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
